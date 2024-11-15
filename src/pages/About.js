928120d472bb8896aaffbbca0b28a66b12790b67
Jst Tan <jsttan.inbox@gmail.com>
2024-11-15 23:22:08 +0800

    Update About.js
    The page contents is still being worked on, a new version of texts will be pushed once ready.

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,23 +10,46 @@ const About = () => {
       <Title level={2} style={{ textAlign: "center" }}>
         About Us
       </Title>
-      <Paragraph style={{ textAlign: "center", color: "#555" }}>
-        Our platform is dedicated to helping you find and join communities that resonate with your interests.
+      <Paragraph style={{ textAlign: "center", color: "#555", fontSize: "16px" }}>
+      Guildiscover is a server listing directory website. Guildiscover is a place for you to discover amazing communities of your interest alike. Guildiscover is a place for all Discord users, for server owner and users alike.
       </Paragraph>
-
       <Row gutter={[16, 16]} style={{ marginTop: "40px" }}>
         <Col xs={24} md={12}>
           <Card title="Our Mission" bordered={false}>
             <Paragraph>
-              To create a welcoming space where users can connect, share, and grow with communities worldwide.
+              Guildiscover mission is to build 
             </Paragraph>
           </Card>
         </Col>
         <Col xs={24} md={12}>
           <Card title="Our Vision" bordered={false}>
             <Paragraph>
-              To empower individuals by providing a platform where they can find like-minded people and thrive
-              together.
+              Guildiscover vision is to create a place where all members could discover amazing communities suited for their interests in the Internet. 
+            </Paragraph>
+          </Card>
+        </Col>
+      </Row>
+      <br></br>
+      <hr></hr>
+      <br></br>
+      <Title level={2} style={{ textAlign: "center" }}>
+        Our Speciality 
+      </Title>
+      <Paragraph style={{ textAlign: "center", color: "#555", fontSize: "16px" }}>
+      Guildiscover is a server listing directory website. Guildiscover is a place for you to discover amazing communities of your interest alike. Guildiscover is a place for all Discord users, for server owner and users alike.
+      </Paragraph>
+      <Row gutter={[16, 16]} style={{ marginTop: "40px" }}>
+        <Col xs={24} md={12}>
+          <Card title="Free Publicity" bordered={false}>
+            <Paragraph>
+              Guildiscover is a place for you to publicise your communities to all of our users. 
+            </Paragraph>
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          <Card title="Good Moderation" bordered={false}>
+            <Paragraph>
+              Guildiscover 
             </Paragraph>
           </Card>
         </Col>
